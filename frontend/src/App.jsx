77a14f00657d9a2b6175a14bbbf7ea0a6e1016d5
Sharib Kabani <ksharib05@gmail.com>
2025-03-17 17:16:43 -0400

    changed to javascript

---
+++ frontend/src/App.jsx
@@ -0,0 +1,6 @@
+function App() {
+  // this should be blue and in the cneter if ur app is working
+	return <h1 className="flex justify-center items-center h-screen text-blue-500">Hello</h1>;
+}
+
+export default App;
