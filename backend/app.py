628ee8eef6381d198b200e7a6cf9db6f93122cd7
2linm <2linmike@gmail.com>
2025-03-30 10:13:43 -0400

    added web context /laurierconnect

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,9 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET'])
+@app.route('/laurierconnect', methods=['GET'])
 def server():
-    return jsonify({'message': 'Hello World!'})
+    return jsonify({'message': 'Hello LaurierConnect!'})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
